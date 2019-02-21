ceaa59ac1e3147a2d3a0e7c221a61d2615ccc868
Zoe Bai <s.xue.bai@gmail.com>
2019-02-21 21:52:51 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,14 @@
+require 'pry'
 class Owner
   # code goes here
   
-  pets = {fishes: [], cats: [], dogs: []}
-  
+  #pets = {fishes: [], cats: [], dogs: []}
+
   @@all = []
   attr_accessor :pets
   attr_reader :name
   def initialize(pets)
-    @pets = pets
+    @pets = {:fishes => [], :dogs => [], :cats => []}
     @@all << self
   end
   
@@ -35,8 +36,59 @@ class Owner
     @name = name
   end
   
-  def pets
-    @pets
+  
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+  
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+  
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+  
+  
+  def sell_pets
+  
+    @pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
+    
+    @pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    
+    @pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    
+    @pets.each do |type, pets|
+      @pets[type] = []
+    end
+  end
+  
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
   
 end
